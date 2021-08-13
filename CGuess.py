71be86f9c4400665e84711f394410b36aceea9f7
drive5bit <88856373+drive5bit@users.noreply.github.com>
2021-08-13 10:47:02 -0700

    Update CGuess.py

--- CGuess.py
+++ CGuess.py
@@ -1,26 +1,30 @@
 # Replit Username: xXEradicationXx
 # Github Username: drive5bit
+# Discord Username: BurningAsh#3369
 
 import time
 import random
 def Guess():
     try:
-
+        name = input("What's your name?: ")
+        print(f"Hi {name}! Welcome to my game. You'll have to self learn this game and it's really easy! After 3 or 4 runs, you'll be great. Good luck!")
         a = int(input('Enter the number limit. Syntax(0 - '))
         while a < 0:
           print("That number is under zero, and that's not allowed. Please try again!")
           a = int(input('Enter the number limit. Syntax(0 - '))
         while a > 100000000000000:
-          b = input("That number is so high that this game will take forever. Are you       sure? Y/N: ").lower()
+          b = input("That number is so high that this game will take forever. Are you Sure? Y/N: ").lower()
           c = ['y', 'n']
           while b not in c:
             print("Our program does not recognize the value you entered. Try again.")
-            b = input("That number is so high that this game will take forever. Are you       sure? Y/N: ").lower()
-            if b  == 'y':
+            b = input("That number is so high that this game will take forever. Are you sure? Y/N: ").lower()
+          if b  == 'y':
               print('Okay. The value was intercepted as response 200.')
-            if b == 'n':
-              quit("We are killing the program. Run the program again to play. Don't        forget to put your value beneath 100000000000000!")
-        secret = int(input(f"What is the secret number now? It has to be between 0 and      {a}  : "))
+              break
+          if b == 'n':
+               quit("We are killing the program. Run the program again to play. Don't forget to put your value beneath 100000000000000!")
+               quit()
+        secret = int(input(f"What is the secret number now? It has to be between 0 and {a}: "))
         while secret < 0 or secret > a:
           if secret < 0:
             print("That number is under zero, and that's not allowed. Please try    again!")
@@ -33,7 +37,7 @@ def Guess():
         while computer != secret:
           computer = random.randint(0, a)
         endTime = time.time()
-        print(f"The computer successfully guessed your number. It guessed {str(computer)    }   and your number was {str(secret)}. The time taken for the computer to get         this result was {endTime - startTime} seconds!")
+        print(f"The computer successfully guessed your number. It guessed {str(computer)} and your number was {str(secret)}. The time taken for the computer to get this result was {endTime - startTime} seconds!")
         again = input("Do you want to play the game again? Y/N: ").lower()
         while again not in ['y', 'n']:
           print("Our programs aren't responding to the value you gave. Plase try    again.")
